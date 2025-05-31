4e49b1c6858eed8b2e2e3774d3d68d9f3f2e27fe
Keith Thibodeaux <keith@railscoder.net>
2025-05-31 11:16:12 -0400

    use faction names

--- factions/factions.go
+++ factions/factions.go
@@ -11,11 +11,11 @@ type Faction struct {
 }
 
 var (
-	Blue   = Faction{Name: "blue", Color: colors.DieReactor}
-	Green  = Faction{Name: "green", Color: colors.DieShield}
-	Orange = Faction{Name: "orange", Color: colors.DieDamage}
-	Purple = Faction{Name: "purple", Color: colors.DieExtra}
-	Yellow = Faction{Name: "yellow", Color: colors.DieThruster}
+	Blue   = Faction{Name: "Komek", Color: colors.DieReactor}
+	Green  = Faction{Name: "Henko", Color: colors.DieShield}
+	Orange = Faction{Name: "Magnomi", Color: colors.DieDamage}
+	Purple = Faction{Name: "Sorelia", Color: colors.DieExtra}
+	Yellow = Faction{Name: "Ventus", Color: colors.DieThruster}
 )
 
 func All() []Faction {
