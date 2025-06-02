a5063bc4b00295ec784cfd8d3246c59b6288e678
Keith Thibodeaux <keith@railscoder.net>
2025-06-02 07:23:52 -0400

    games use language

--- games/actions.go
+++ games/actions.go
@@ -9,7 +9,7 @@ import (
 func (s *Game) SetFaction(player *Player, faction *factions.Faction) error {
 	return s.withLock(func() error {
 		if faction == nil {
-			return errors.New("Faction cannot be nil")
+			return errors.New("error.faction_cannot_be_nil")
 		}
 
 		player.Faction = faction
@@ -21,7 +21,7 @@ func (s *Game) AddPoints(pName string, amount int) error {
 	return s.withLock(func() error {
 		player, exists := s.getPlayer(pName)
 		if !exists {
-			return errors.New("Player not found")
+			return errors.New("error.player_not_found")
 		}
 		player.incrementPoints(amount)
 		return nil
