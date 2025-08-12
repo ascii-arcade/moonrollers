6a0e69aa7d563e5f4784ef287c44af0239687a55
Keith Thibodeaux <keith@railscoder.net>
2025-08-12 09:57:37 -0400

    use string builder

--- board/input_stage_choose_crew_component.go
+++ board/input_stage_choose_crew_component.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	"github.com/ascii-arcade/moonrollers/keys"
-	"github.com/charmbracelet/lipgloss"
 )
 
 type inputStageChooseCrewComponent struct {
@@ -19,26 +18,22 @@ func newInputStageChooseCrewComponent(model *Model) inputStageChooseCrewComponen
 }
 
 func (c inputStageChooseCrewComponent) render() string {
-	header := "Choose Crew"
-	if c.model.Game.InputCrew != nil {
-		header = c.model.style.Bold(true).Foreground(c.model.Game.InputCrew.Faction.Color).Render(c.model.Game.InputCrew.Name)
+	var output strings.Builder
+	if c.model.Game.InputCrew == nil {
+		output.WriteString("Choose Crew")
+	} else {
+		output.WriteString(c.model.style.Bold(true).Foreground(c.model.Game.InputCrew.Faction.Color).Render(c.model.Game.InputCrew.Name))
 	}
+	output.WriteString("\n\n")
 
-	var crewList []string
 	for index, crew := range c.model.Game.CrewForHire {
-		text := fmt.Sprintf("[%d] %s", index+1, crew.Name)
-		crewList = append(crewList, c.model.style.Foreground(crew.Faction.Color).Render(text))
+		text := fmt.Sprintf("[%d] %s\n", index+1, crew.Name)
+		output.WriteString(c.model.style.Foreground(crew.Faction.Color).Render(text))
 	}
 
-	confirm := ""
 	if c.model.Game.InputCrew != nil {
-		confirm = fmt.Sprintf("%s to confirm", keys.GameChooseConfirm.String(c.model.style))
+		output.WriteString(fmt.Sprintf("\n\n%s to confirm", keys.GameChooseConfirm.String(c.model.style)))
 	}
 
-	return inputComponentStyle(false).Render(lipgloss.JoinVertical(
-		lipgloss.Top,
-		header+"\n\n",
-		strings.Join(crewList, "\n"),
-		"\n\n"+confirm,
-	))
+	return inputComponentStyle(false).Render(output.String())
 }
